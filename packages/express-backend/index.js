2fbc9ccfc0522efc62719970a677d18883206895
CandyCaneCazoo <aiden.j.king004@gmail.com>
2025-01-13 10:27:29 -0800

    Added id search route

--- packages/express-backend/index.js
+++ packages/express-backend/index.js
@@ -46,8 +46,7 @@ const users = {
     ]
 };
 
-// GET Users
-
+// GET Users by filters
 const findUserByName = (name) => {
     return users["users_list"].filter(
       (user) => user["name"] === name
@@ -62,5 +61,19 @@ app.get("/users", (req, res) => {
       res.send(result);
     } else {
       res.send(users);
+    }
+});
+
+// GET Users by id
+const findUserById = (id) =>
+    users["users_list"].find((user) => user["id"] === id);
+  
+app.get("/users/:id", (req, res) => {
+    const id = req.params["id"]; //or req.params.id
+    let result = findUserById(id);
+    if (result === undefined) {
+      res.status(404).send("Resource not found.");
+    } else {
+      res.send(result);
     }
 });
